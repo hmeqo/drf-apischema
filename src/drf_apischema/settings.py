25078cf1556473fb4e3ac98665b1b4846a1fc4c8
hmeqo <hmeqocoliniliad@gmail.com>
2024-11-20 05:29:30 +0800

    0.1.10

--- src/drf_apischema/settings.py
+++ src/drf_apischema/settings.py
@@ -2,10 +2,15 @@ from django.conf import settings
 
 
 class ApiSettings:
-    def transaction(self, override: bool | None) -> bool:
+    def transaction(self, override: bool | None = None) -> bool:
         if override is not None:
             return override
         return getattr(settings, "DRF_APISPEC_TRANSACTION", True)
+
+    def sqllogger(self, override: bool | None = None) -> bool:
+        if override is not None:
+            return override
+        return getattr(settings, "DRF_APISPEC_SQLLOGGER", True)
 
 
 apisettings = ApiSettings()
