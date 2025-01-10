48bbf4525bcc87a68cc61fe6cb9a1e46e81feefe
hmeqo <hmeqou@gmail.com>
2025-01-11 05:36:08 +0800

    0.1.14

--- tests/api/views.py
+++ tests/api/views.py
@@ -29,7 +29,8 @@ class TestViewSet(GenericViewSet):
     def square(self, request: ASRequest[SquareQuery]):
         """Square a number"""
         # request.serializer is instance of BQuery that is validated
-        print(request.serializer)
+        # print(request.serializer)
+
         # request.validated_data is serializer.validated_data
         n: int = request.validated_data["n"]
-        return SquareOut({"result": n * 2}).data
+        return SquareOut({"result": n * n}).data
